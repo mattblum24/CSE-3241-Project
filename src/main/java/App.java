594e54069f5fe44ab9dc56f2bd12f40b10535ab7
mattblum24 <157322386+mattblum24@users.noreply.github.com>
2024-10-02 12:04:47 -0400

    Added a prototype for login

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,25 @@
+inport java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Welcome to ___ Rentals!");
+        System.out.println("Do you have an account?");
+        String yesOrNo = scan.nextLine();
+        if(yesOrNo = "yes") {
+            System.out.println("Username: ");
+            String username = scan.nextLine();
+            System.out.println("Password: ");
+            String password = scan.nextLine();
+        } else {
+            System.out.println("Username: ");
+            String username = scan.nextLine();
+            System.out.println("Password: ");
+            String password = scan.nextLine();
+            System.out.println("Email: ");
+            String email = scan.nextLine();
+            System.out.println("Phone Number: ");
+            String phoneNumber = scan.nextLine();
+        }
     }
 }
