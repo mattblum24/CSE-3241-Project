b9ff1b108cd8cdbac496c2a8952c46e57f098dc0
mattblum24 <157322386+mattblum24@users.noreply.github.com>
2024-10-02 12:40:12 -0400

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,25 +1,54 @@
-inport java.util.Scanner;
+import java.util.Scanner;
 
 public class App {
+    
+    // Runs the login menu
+    public static void loginOrSignup(Scanner scan, String yesOrNo) {
+        if(yesOrNo.equals("yes")) {
+            String username = loginUsername(scan);
+            String password = loginPassword(scan);
+        } else {
+            String username = loginUsername(scan);
+            String password = loginPassword(scan); 
+            String email = signupEmail(scan);
+            String phoneNumber = signupPhoneNumber(scan);
+        }
+    }
+    
+    // Gathers the username from the user
+    public static String loginUsername(Scanner scan) {
+        System.out.println("Username: ");
+        String username = scan.nextLine(); 
+        return username;
+    }
+
+    // Gathers the password from the user
+    public static String loginPassword(Scanner scan) {
+        System.out.println("Password: ");
+        String password = scan.nextLine();
+        return password;
+    }
+
+    // Gathers the email from the user
+    public static String signupEmail(Scanner scan) {
+        System.out.println("Email: ");
+        String email = scan.nextLine();
+        return email;
+    }
+    
+    // Gathers the phone number from the user
+    public static String signupPhoneNumber(Scanner scan) {
+        System.out.println("Phone Number: ");
+        String phoneNumber = scan.nextLine();
+        return phoneNumber;
+    }
+    
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
         System.out.println("Welcome to ___ Rentals!");
         System.out.println("Do you have an account?");
         String yesOrNo = scan.nextLine();
-        if(yesOrNo = "yes") {
-            System.out.println("Username: ");
-            String username = scan.nextLine();
-            System.out.println("Password: ");
-            String password = scan.nextLine();
-        } else {
-            System.out.println("Username: ");
-            String username = scan.nextLine();
-            System.out.println("Password: ");
-            String password = scan.nextLine();
-            System.out.println("Email: ");
-            String email = scan.nextLine();
-            System.out.println("Phone Number: ");
-            String phoneNumber = scan.nextLine();
-        }
+        loginOrSignup(scan, yesOrNo);
+        scan.close();
     }
 }
