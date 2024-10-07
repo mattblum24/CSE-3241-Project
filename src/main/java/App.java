732cf0244d7ab1201970448c44481ef0cb916f92
mattblum24 <Matt.blum04@gmail.com>
2024-10-07 17:23:00 -0400

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,12 @@
 import java.util.Scanner;
-
 import entities.Drone;
 import entities.Equipment;
-
 import java.util.HashMap;
 
 public class App {
 
-    HashMap<String, Equipment> equipmentRepository = new HashMap<String, Equipment>();
-    HashMap<String, Drone> droneRepository = new HashMap<String, Drone>();
+    HashMap<String, Equipment> equipmentRepository = new HashMap<>();
+    HashMap<String, Drone> droneRepository = new HashMap<>();
     
     // Runs the login menu
     public static void loginOrSignup(Scanner scan, String yesOrNo) {
@@ -26,57 +24,56 @@ public class App {
     // Gathers the username from the user
     public static String loginUsername(Scanner scan) {
         System.out.println("Username: ");
-        String username = scan.nextLine(); 
-        return username;
+        return scan.nextLine();
     }
 
     // Gathers the password from the user
     public static String loginPassword(Scanner scan) {
         System.out.println("Password: ");
-        String password = scan.nextLine();
-        return password;
+        return scan.nextLine();
     }
 
     // Gathers the email from the user
     public static String signupEmail(Scanner scan) {
         System.out.println("Email: ");
-        String email = scan.nextLine();
-        return email;
+        return scan.nextLine();
     }
     
     // Gathers the phone number from the user
     public static String signupPhoneNumber(Scanner scan) {
         System.out.println("Phone Number: ");
-        String phoneNumber = scan.nextLine();
-        return phoneNumber;
+        return scan.nextLine();
     }
    
-    // main menu
+    // Main menu
     public static int mainMenu(Scanner scan) {
         System.out.println("Type 1 to see previous orders, type 2 to see available equipment to rent or type 3 to exit: ");
-        int input = scan.nextInt();
-        return input;
-
+        if(!scan.hasNextInt()) {
+            scan.next(); // Discard invalid input
+            return 3;
+        }
+        return scan.nextInt();
     }
 
-    // previous orders menu
-    public static void previousOrders(Scanner scan) {    
+    // Previous orders menu
+    public static void previousOrders(Scanner scan) {
         System.out.println("Which order number would you like to leave a review for?");
         int orderNumber = scan.nextInt();
+        scan.nextLine(); // Consume newline left-over
         String str = "Review for order %d: ";
-        String strOutput = String.format(str, orderNumber);
-        System.out.println(strOutput);
+        System.out.printf(str, orderNumber);
         String userReview = scan.nextLine();
+        System.out.println("Thank you for your review!");
     }
 
-    // avaliable equipment menu
-    public static void avaliableEquipment(Scanner scan) {
-        System.out.println("Type in the serial number for piece of equipment you would like to rent: ");
+    // Available equipment menu
+    public static void availableEquipment(Scanner scan) {
+        System.out.println("Type in the serial number for the piece of equipment you would like to rent: ");
         int serialNumber = scan.nextInt();
         if(serialNumber > 0) {
-            System.out.println("Thanks for your order! The estimated arrival is ______"); 
+            System.out.println("Thanks for your order! The estimated arrival is ______");
         }
-    };
+    }
 
     public static void main(String[] args) throws Exception {
         Scanner scan = new Scanner(System.in);
@@ -84,25 +81,28 @@ public class App {
         System.out.println("Do you have an account? (yes or no)");
         String yesOrNo = scan.nextLine();
         loginOrSignup(scan, yesOrNo);
+
         int menu = mainMenu(scan);
+        scan.nextLine(); // Consume newline
+
         while(menu < 3) {
             if(menu == 1) {
                 System.out.println("Would you like to review? (yes or no)");
                 String review = scan.nextLine();
-                if(review.equals("yes")) {;
-                    previousOrders(scan); 
+                if(review.equals("yes")) {
+                    previousOrders(scan);
                 }
             } else if(menu == 2) {
-                
                 System.out.println("Would you like to rent a piece of equipment? (yes or no)");
                 String enter = scan.nextLine();
                 if(enter.equals("yes")) {
-                    avaliableEquipment(scan); 
+                    availableEquipment(scan);
                 }
             }
-            menu = mainMenu(scan); 
+            menu = mainMenu(scan);
+            scan.nextLine(); // Consume newline left-over
         }
-        System.out.println("Thank you for visiting ___ rentals! Hope to see you again soon!");
+        System.out.println("Thank you for visiting ___ Rentals! Hope to see you again soon!");
         scan.close();
     }
 }
